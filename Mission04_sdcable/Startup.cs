2ec5e0965f0ab3b7169b2845b49ab42b4a889fee
msajkld <sdcable18@gmail.com>
2023-01-28 11:31:36 -0700

    Finished Product

--- Mission04_sdcable/Startup.cs
+++ Mission04_sdcable/Startup.cs
@@ -21,20 +21,22 @@ namespace Mission04_sdcable
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
---
+++ Mission04_sdcable/wwwroot/js/index.js
@@ -0,0 +1,54 @@
+﻿$("#submitButton").click(function () { //Receive values of the form onclick of the submit button
+    assignments = ($("#assignments").val()) * .5
+    group = ($("#groupProject").val()) * .1
+    quizzes = ($("#quizzes").val()) * .1
+    midterm = ($("#midtermExam").val()) * .1
+    final = ($("#finalExam").val()) * .1
+    INTEX = ($("#INTEX").val()) * .1
+
+    //Calculate Overall %
+    output = assignments + group + quizzes + midterm + final + INTEX
+
+    //Calculate what Grade letter they received
+    if (output >= 94) {
+        outputLetter = 'A'
+    }
+    else if (output >= 90) {
+        outputLetter = 'A-'
+    }
+    else if (output >= 87) {
+        outputLetter = 'B+'
+    }
+    else if (output >= 84) {
+        outputLetter = 'B'
+    }
+    else if (output >= 80) {
+        outputLetter = 'B-'
+    }
+    else if (output >= 77) {
+        outputLetter = 'C+'
+    }
+    else if (output >= 74) {
+        outputLetter = 'C'
+    }
+    else if (output >= 70) {
+        outputLetter = 'C-'
+    }
+    else if (output >= 67) {
+        outputLetter = 'D+'
+    }
+    else if (output >= 64) {
+        outputLetter = 'D'
+    }
+    else if (output >= 60) {
+        outputLetter = 'D-'
+    }
+    else {
+        outputLetter = 'E'
+    }
+
+    //output values to respective divs in index.html
+    $("#outputPercent").text("Your overall grade percent is: " + output + "%")
+    $("#outputLetter").text("Your letter grade is: " + outputLetter)
+
+})
